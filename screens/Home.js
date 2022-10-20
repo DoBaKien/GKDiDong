6096ec6e9f713477ca5e0a11a595a981a372f224
DoBaKien <kiendoba1905@gmail.com>
2022-10-20 09:54:54 +0700

    screen1

---
+++ screens/Home.js
@@ -0,0 +1,75 @@
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import data from "../data";
+import List from "./List";
+import Icon from "react-native-vector-icons/Feather";
+function Home() {
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          flexDirection: "column",
+        },
+      ]}
+    >
+      <View>
+        <Text style={styles.title}>Find Best Burger in Your City</Text>
+        <View flexDirection="row">
+          <TextInput style={styles.input} placeholder="Search..." />
+          <Icon
+            name="search"
+            size={30}
+            color="#900"
+            style={styles.iconSearch}
+          />
+        </View>
+        <Text style={styles.title}>Near You now</Text>
+      </View>
+      <View style={{ flex: 1 }}>
+        <FlatList
+          numColumns={2}
+          keyExtractor={(data) => data.key}
+          data={data}
+          renderItem={(data) => <List a={data.item} />}
+        />
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  iconSearch: {
+    marginTop: 15,
+    marginLeft: 50,
+    backgroundColor: "red",
+    height: 40,
+    width: 40,
+    textAlign: "center",
+    paddingTop: 5,
+    borderRadius: 30,
+  },
+  container: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: 15,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 250,
+    borderRadius: 20,
+  },
+});
+
+export default Home;
---
+++ screens/List.js
@@ -0,0 +1,58 @@
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableWithoutFeedback,
+} from "react-native";
+
+function List({ a }) {
+  return (
+    <TouchableWithoutFeedback onPress={() => console.log(a.key)}>
+      <View style={styles.container}>
+        <Image style={styles.tinyLogo} source={require("../Image/b.png")} />
+        <View flexDirection="row">
+          <View>
+            <Text style={{ marginRight: 10 }}>{a.name}</Text>
+            <Text>{a.price}</Text>
+          </View>
+          <View style={{ marginTop: 10, marginLeft: 20 }}>
+            <Image
+              style={{ width: 20, height: 20, resizeMode: "contain" }}
+              source={require("../Image/va.png")}
+            />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  star: {
+    paddingLeft: 10,
+    paddingTop: 10,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  content: {
+    alignItems: "center",
+  },
+  tinyLogo: {
+    resizeMode: "stretch",
+    height: 100,
+    width: 150,
+  },
+  container: {
+    marginTop: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    flex: 1,
+    display: "flex",
+    alignItems: "center",
+    borderRadius: 10,
+    backgroundColor: "lightgray",
+  },
+});
+
+export default List;
